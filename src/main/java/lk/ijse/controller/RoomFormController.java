f51ac91bb6439dd012096684137689cbfe4ac831
Isanka Suresh <isankasuresh001@gmail.com>
2023-09-07 23:02:26 +0530

    add room controller

---
+++ src/main/java/lk/ijse/controller/RoomFormController.java
@@ -0,0 +1,64 @@
+package lk.ijse.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class RoomFormController {
+
+    @FXML
+    private TextField txtRoomId;
+
+    @FXML
+    private ComboBox<?> cmbType;
+
+    @FXML
+    private TextField txtKeyMoney;
+
+    @FXML
+    private TextField txtQuantity;
+
+    @FXML
+    private JFXButton btnAddNew;
+
+    @FXML
+    private JFXButton btnSearch;
+
+    @FXML
+    private JFXButton btnUpdate;
+
+    @FXML
+    private JFXButton btnDelete;
+
+    @FXML
+    private TableView<?> tblRoom;
+
+    @FXML
+    void btnAddNewOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSearchOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cmbTypeOAction(ActionEvent event) {
+
+    }
+
+}
