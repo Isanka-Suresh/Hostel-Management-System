a6e627d1e38c980446eb5eb90da00203023c3a19
Isanka Suresh <isankasuresh001@gmail.com>
2023-09-07 22:54:27 +0530

    add RoomBO

---
+++ src/main/java/lk/ijse/bo/custom/RoomBO.java
@@ -0,0 +1,14 @@
+package lk.ijse.bo.custom;
+
+import lk.ijse.bo.SuperBO;
+import lk.ijse.dto.RoomDTO;
+import lk.ijse.entity.Room;
+
+import java.util.List;
+
+public interface RoomBO extends SuperBO {
+    public List<RoomDTO> getAllRoom() throws Exception;
+    public boolean addRoom(RoomDTO room) throws Exception;
+    public boolean updateRoom(RoomDTO room) throws Exception;
+    public boolean deleteRoom(String id) throws Exception;
+}
