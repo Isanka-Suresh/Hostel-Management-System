2ce3cb1dbc410845f3d59c8b039ac4a0cfb1bd77
Isanka Suresh <isankasuresh001@gmail.com>
2023-09-07 22:57:17 +0530

    add ReservationTM

---
+++ src/main/java/lk/ijse/tm/ReservationTM.java
@@ -0,0 +1,15 @@
+package lk.ijse.tm;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+@Data
+@AllArgsConstructor
+public class ReservationTM {
+    private String reservationId;
+    private String studentId;
+    private String roomId;
+    private LocalDate date;
+    private boolean status;
+}
