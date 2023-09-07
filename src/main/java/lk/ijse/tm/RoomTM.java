ed2a2f9ef66ac492edc5704f7616e2c8182496ec
Isanka Suresh <isankasuresh001@gmail.com>
2023-09-07 22:55:30 +0530

    add RoomTM

---
+++ src/main/java/lk/ijse/tm/RoomTM.java
@@ -0,0 +1,10 @@
+package lk.ijse.tm;
+
+import java.time.LocalDate;
+
+public class RoomTM {
+    private String roomTypeId;
+    private String roomType;
+    private double keyMoney;
+    private int quantity;
+}
