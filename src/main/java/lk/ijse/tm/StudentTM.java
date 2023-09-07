982175df4dcefa33222db648fe4995e519d43d00
Isanka Suresh <isankasuresh001@gmail.com>
2023-09-07 22:53:52 +0530

    add StudentTM

---
+++ src/main/java/lk/ijse/tm/StudentTM.java
@@ -0,0 +1,12 @@
+package lk.ijse.tm;
+
+import java.time.LocalDate;
+
+public class StudentTM {
+    private String studentId;
+    private String name;
+    private String address;
+    private String contact;
+    private LocalDate dob;
+    private boolean isMale;
+}
