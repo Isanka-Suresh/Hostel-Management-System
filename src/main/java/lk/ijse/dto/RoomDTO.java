40e061d5cb11b413dcf326974aa0eaa585145334
Isanka Suresh <isankasuresh001@gmail.com>
2023-09-07 22:55:15 +0530

    add RoomDTO

---
+++ src/main/java/lk/ijse/dto/RoomDTO.java
@@ -0,0 +1,52 @@
+package lk.ijse.dto;
+
+import java.io.Serializable;
+
+public class RoomDTO implements Serializable {
+    private String roomTypeId;
+    private String roomType;
+    private double keyMoney;
+    private int quantity;
+
+    public RoomDTO() {
+    }
+
+    public RoomDTO(String roomTypeId, String roomType, double keyMoney, int quantity) {
+        this.roomTypeId = roomTypeId;
+        this.roomType = roomType;
+        this.keyMoney = keyMoney;
+        this.quantity = quantity;
+    }
+
+    public String getRoomTypeId() {
+        return roomTypeId;
+    }
+
+    public void setRoomTypeId(String roomTypeId) {
+        this.roomTypeId = roomTypeId;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public double getKeyMoney() {
+        return keyMoney;
+    }
+
+    public void setKeyMoney(double keyMoney) {
+        this.keyMoney = keyMoney;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
