611a3f96f3b2ee3bf01587a2ea882fb54dba334c
Isanka Suresh <isankasuresh001@gmail.com>
2023-09-07 22:51:35 +0530

    add StudentBO

---
+++ src/main/java/lk/ijse/bo/custom/StudentBO.java
@@ -0,0 +1,14 @@
+package lk.ijse.bo.custom;
+
+import lk.ijse.bo.SuperBO;
+import lk.ijse.dto.StudentDTO;
+import lk.ijse.entity.Student;
+
+import java.util.List;
+
+public interface StudentBO extends SuperBO {
+    public List<StudentDTO> getAllStudent() throws Exception;
+    public boolean addStudent(StudentDTO student) throws Exception;
+    public boolean updateStudent(StudentDTO student) throws Exception;
+    public boolean deleteStudent(String id) throws Exception;
+}
